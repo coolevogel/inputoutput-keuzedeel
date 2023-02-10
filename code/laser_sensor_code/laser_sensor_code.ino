784c5a9e15f0ebeb6900131523b276e9c6332711
mathtijs <matthijsjurriaans333@gmail.com>
2023-02-10 11:12:40 +0100

    alles klaar

--- code/laser_sensor_code/laser_sensor_code.ino
+++ code/laser_sensor_code/laser_sensor_code.ino
@@ -1,29 +1,27 @@
-#define DETECT 2 // pin 2 for  sensor
-#define ACTION 8 // pin 8 for action to do someting
+#define DETECT 2
+#define ACTION 8
 
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Matthijs Jurriaans");
-  pinMode(DETECT, INPUT);//define detect input pin
-  pinMode(ACTION, OUTPUT);//define ACTION output pin
+  pinMode(DETECT, INPUT);
+  pinMode(ACTION, OUTPUT);
 
 }
 
 void loop() {
 
-  int detected = digitalRead(DETECT);// read Laser sensor
-  Serial.println(detected);
+  int detected = digitalRead(DETECT);
 
  
   if( detected == HIGH)
   {
-    digitalWrite(ACTION,HIGH);// set the buzzer ON
-    // Serial.println("Detected!");
+    digitalWrite(ACTION,HIGH);
+    Serial.println("Detected!");
 
   }else{
-    digitalWrite(ACTION,LOW); // Set the buzzer OFF
-    // Serial.println("No laser");
+    digitalWrite(ACTION,LOW); 
+    Serial.println("No laser");
 
   }
   
